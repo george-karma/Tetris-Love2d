0a95a7719ab1a09eee876bcbc14abc143376a1d6
George Nitu <danut.nitu@outlook.com>
2019-06-25 16:50:00 +0100

    Tween library does not work at the moment

--- obj/ExplosionFX.lua
+++ obj/ExplosionFX.lua
@@ -2,21 +2,23 @@ local ExplosionFX = Class:extend()
 
 function ExplosionFX:new(interface,x,y,opts)
 	self.interface = interface
-	self.x, self.y = x,yLocation
+	self.x, self.y = x,y
 	self.timer = Timer()
 	self.order = 5
-	colour = opts.colour or colours_array["b"]
-	rotation = opts.rotation or love.math.random(0,2*math.pi)
+	print("alive")
+	self.colour = opts.colour or colours_array["b"]
+	self.rotation = opts.rotation or love.math.random(0,2*math.pi)
 	self.size = opts.size or love.math.random(2,3)
 	self.velocity = opts.size or love.math.random(75,150)
 	self.line_width = opts.line_width or love.math.random(2,4)
 	collider = self.interface.world:newCircleCollider(self.x, self.y,1)
 	collider:setObject(self)
-	collider:setLinearVelocity(self.velocity*math.cos(rotation), self.velocity*math.sin(rotation))
+	collider:setLinearVelocity(self.velocity*math.cos(self.rotation), self.velocity*math.sin(self.rotation))
 	self.timer:tween(opts.time or love.math.random(0.3,0.5), self,{size = 0, velocity = 0, line_width = 0},
 						"linear", function()
-							self.dead = true
 							print("Exploded")
+							self.dead = true
+							
 							end)
 end
 
@@ -27,7 +29,7 @@ end
 
 
 function ExplosionFX:draw()
-	
+	localRotation(self.x,self.y,rotation)
 end
 
 function localRotation(xLocation,yLocation,rotation)
@@ -35,9 +37,11 @@ function localRotation(xLocation,yLocation,rotation)
 	love.graphics.translate(xLocation,yLocation)
 	love.graphics.rotate(rotation or 0)
 	love.graphics.translate(-xLocation, -yLocation)
+	love.graphics.pop()
 end
 
 function random(a,b)
 	return love.math.random(a,b)
 end
+
 return ExplosionFX
